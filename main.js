80de9d5572b270243fecb7df47bb7b334276417d
Melany Contreras <melanycontreras164@gmail.com>
2024-08-08 12:09:13 -0300

    Agregue el trabajo al git hub

--- main.js
+++ main.js
@@ -1,43 +1,39 @@
 import * as bootstrap from 'bootstrap'; // Los script de bootstrap
 import "./style.css";
+import Handlebars from "handlebars";
+import "./style.css";
 
+const urlPlantilla = 'template/listadoProductos.hbs';
+const urlBlackProductos = 'http://localhost:8080/productos/';
 
-function getLocalStorage() {
-    console.log(localStorage)
+const obtenerProductosYMostrarlos = async () => {
+    try {
+        const res = await fetch(urlPlantilla);
+
+        if (!res.ok) {
+            throw new Error('No se pudo obtener la plantilla');
+        }
+
+        const plantilla = await res.text();
+        console.log(plantilla);
+
+        const productosRes = await fetch(urlBlackProductos);
+        if (!productosRes.ok) {
+            throw new Error('No se pudieron obtener los productos');
+        }
+
+        const productos = await productosRes.json();
+        console.log(productos);
+
+        const template = Handlebars.compile(plantilla);
+
+        const htmlListo = template({ productos });
+        console.log(htmlListo);
+        
+        // Aquí podrías insertar el HTML generado en el DOM
+        document.getElementById('contenedor-productos').innerHTML = htmlListo;
+    } catch (error) {
+        console.error('Error:', error);
+    }
 }
-
-
-
-
-function getSessionStorage(){
-    console.log(SessionStorage)
-}
-
-
-function getLocalStorageItem(clave){
-    const valor = localStorage.getItem(clave)
-    console.log(valor)
- } 
-
- /* GET ITEM */
-
-//console.log(LocalStorage.getItem('nombre')) 
-
-//getLocalStorage('nombre')
-//getLocalStorage('edad')
-//getLocalStorage('isTeacher')
-
-/* setItem */
-
-//localStorage(<key>, <value>)
-localStorage.setItem('numero', 2) // dsnajfanao
-localStorage.setItem('booleano', true) // El true lo va a convertir en un string
-
-/* localStorage.setItem('array',[2, 4, 5, 6, 7]) */
-
-localStorage.setItem('array',[2, 4, 5, 6, 7])
-
-
-
-
 
